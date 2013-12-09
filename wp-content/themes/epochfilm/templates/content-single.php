4d23bd4a05db3ad3083789beac3cc0875a37532d
Denis <at10tion.angry@yandex.ua>
2013-12-09 16:11:51 +0200

    временно добавил кнопицы соц сетей в singl-тэмплэйт

--- wp-content/themes/epochfilm/templates/content-single.php
+++ wp-content/themes/epochfilm/templates/content-single.php
@@ -10,6 +10,18 @@
 			<div id="left-column-details" class="left-column-details">
 				<div class="details-header">
 					<h3>Оригинальное название: <?= $custom_fields['eng_name'][0] ?></h3>
+					<div id="socials">
+						<script type="text/javascript">(function() {
+								if (window.pluso)if (typeof window.pluso.start == "function") return;
+								if (window.ifpluso==undefined) { window.ifpluso = 1;
+									var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+									s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+									s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+									var h=d[g]('body')[0];
+									h.appendChild(s);
+								}})();</script>
+						<div class="pluso" data-background="transparent" data-options="small,round,line,horizontal,counter,theme=04" data-services="vkontakte,odnoklassniki,twitter,google,facebook"></div>
+					</div>
 				</div>
 				<div class="content-wrapper-details">
 					<div class="details-poster">
@@ -19,7 +31,6 @@
 					<div class="description-film-container">
 						<div class="header-comments">
 							<h1><?php the_title(); ?></h1>
-
 							<div id="ratingImDb"><?= $custom_fields['rating_plugin_HTML'][0] ?></div>
 						</div>
 						<div class="film-history">
