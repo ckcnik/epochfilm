7da2425f5cc345a8670676dcb04e3ecb54a1949c
Denis <at10tion.angry@yandex.ua>
2013-12-28 14:24:29 +0200

    поменял стили для шфблона films

--- wp-content/themes/epochfilm/templates/content.php
+++ wp-content/themes/epochfilm/templates/content.php
@@ -1,12 +1,17 @@
 <?php
 $custom_fields = get_post_custom($id);
 ?>
-<li class="new-film">
+<li class="new-film box-shadow">
 	<article <?php post_class(); ?>>
 		<div>
 			<header class="video-title">
-				<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-<!--				<time class="year-film">--><?//= getCategory($id, 19); ?><!--</time>-->
+				<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?>
+						<?php
+							$category = get_the_category();
+							echo "(".$category[0]->cat_name.")";
+						?>
+					</a>
+				</h5>
 			</header>
 			<div class="main-poster">
 				<a href="<?php the_permalink(); ?>">
