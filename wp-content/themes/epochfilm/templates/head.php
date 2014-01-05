4116d1bd2b3fe0b67ab6401bc4530b574c1cda6d
Sergey Suhoverhov <geekboox@yandex.ua>
2014-01-05 16:23:00 +0200

    Поправил конфиги.

--- wp-content/themes/epochfilm/templates/head.php
+++ wp-content/themes/epochfilm/templates/head.php
@@ -15,6 +15,6 @@
 	<script type="text/javascript" src="//vk.com/js/api/openapi.js?105"></script>
 
 	<script type="text/javascript">
-		VK.init({apiId: <?= VK_APP_ID ?>, onlyWidgets: true});
+		VK.init({apiId: <?= VK_COMMENT_API_ID ?>, onlyWidgets: true});
 	</script>
 </head>
